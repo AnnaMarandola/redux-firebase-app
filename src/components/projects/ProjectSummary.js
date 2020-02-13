046c748a041c68c359489908973611a93ace9b20
AnnaMarandola <annamarandola@gmail.com>
2020-02-13 10:52:09 +0100

    card css

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Card from './Card.css';
 
 const ProjectSummary = ({project}) => {
     return (
         <div className="card z-depth-0 project-summary">
         <div className="card-content grey-text text-darken-3">
           <span className="card-title">{project.title}</span>
+          <img className="enigme-image" src={project.url}/>
           <p>Posted by Anna</p>
           <p className="grey-text">11 février, 20:20</p>
         </div>
